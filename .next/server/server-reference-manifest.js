4144cb3bd428d9105859dfba06d89dd61099d229
Javlonbk <javlonbeabr41@gmail.com>
2024-12-20 16:25:43 +0500

    setsettled

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qJT1Jb8opzKLtppg1ptAohR4GlEg0Qyx8DTPCnsIysY=\"\n}"
---
+++ .next/static/chunks/app_dashboard_layout_tsx_d1ac8d._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_dashboard_layout_tsx_d1ac8d._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_dashboard_layout_tsx_d1ac8d._.js",
+  "chunks": [
+    "static/chunks/_e47259._.js"
+  ],
+  "source": "dynamic"
+});
